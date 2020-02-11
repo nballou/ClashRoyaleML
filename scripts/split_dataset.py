ee3e5017aa11b4c90cb8686abe315f6858dd6fbe
Sebastian Berns <s.berns@qmul.ac.uk>
2020-02-11 12:01:46 +0000

    Improved data split script

--- scripts/split_dataset.py
+++ scripts/split_dataset.py
@@ -29,15 +29,21 @@ import pandas as pd
 ### SETTINGS
 
 # Random seed
-SEED = 42
+SEED = 999
 np.random.seed(SEED)
 
 # Base directory
 BASE = pathlib.Path.cwd()
 
-# Data dir
+# Data dir and files
 DATADIR = BASE / 'data_clean'
 CSVFILE = 'processed_data.csv'
+SAVEPREFIX = 'processed_split_seed{}'.format(SEED)
+
+FILENAME_TEST_INDEX = '{}_indices_test.csv'.format(SAVEPREFIX)
+FILENAME_TRAIN_INDEX = '{}_indices_train.csv'.format(SAVEPREFIX)
+FILENAME_TEST_DATA = '{}_test.csv'.format(SAVEPREFIX)
+FILENAME_TRAIN_DATA = '{}_train.csv'.format(SAVEPREFIX)
 
 # Percentage of test data
 TESTSPLIT = 20/100.0
@@ -54,37 +60,55 @@ def clock():
 
 ### SCRIPT
 
-print("Ready  ({})".format(clock()))
+print("Ready  ({}) \n".format(clock()))
 
 
 df = pd.read_csv(DATADIR/CSVFILE)
-print("Data imported  ({})".format(clock()))
-
 N = len(df.index)
-print("{} rows".format(N))
+print("Data imported  ({}) \n"\
+    "{} rows \n".format(
+        clock(),
+        N
+    ))
 
 N_test = round(N * TESTSPLIT)
 N_train = N - N_test
 
 index = np.array(df.index.values)
 np.random.shuffle(index)
+print("Shuffled data set  ({}) \n"\
+    "random seed {} \n".format(
+        clock(),
+        SEED
+    ))
+
 index_test = index[:N_test]
 index_train = index[N_test:]
 
 df_test = df.iloc[index_test]
 df_train = df.iloc[index_train]
 
-np.savetxt(DATADIR/'data_split_indices_test.csv', index_test, delimiter=',')
-np.savetxt(DATADIR/'data_split_indices_train.csv', index_train, delimiter=',')
+print("Split data set  ({}) \n"\
+    "{:.0f}% training examples ({}) and \n"\
+    "{:.0f}% test examples ({}) \n".format(
+        clock(),
+        (len(index_train)*1.0/N)*100, len(index_train),
+        (len(index_test)*1.0/N)*100, len(index_test)
+    ))
 
-df_test.to_csv(DATADIR/'data_split_test.csv')
-df_train.to_csv(DATADIR/'data_split_train.csv')
+np.savetxt(DATADIR/FILENAME_TEST_INDEX, index_test, delimiter=',')
+np.savetxt(DATADIR/FILENAME_TRAIN_INDEX, index_train, delimiter=',')
 
-print("Split data set into \n{:.0f}% training examples ({}) and \n{:.0f}% test examples ({})".format(
-    (len(index_train)*1.0/N)*100, len(index_train),
-    (len(index_test)*1.0/N)*100, len(index_test)
-))
-print("({})".format(clock()))
+df_test.to_csv(DATADIR/FILENAME_TEST_DATA)
+df_train.to_csv(DATADIR/FILENAME_TRAIN_DATA)
+
+print("Saved split sets and indices  ({}) \n"\
+    "{} \n"\
+    "{} \n".format(
+        clock(),
+        FILENAME_TEST_DATA,
+        FILENAME_TRAIN_DATA
+    ))
 
 
 print("Done  ({})".format(clock()))
